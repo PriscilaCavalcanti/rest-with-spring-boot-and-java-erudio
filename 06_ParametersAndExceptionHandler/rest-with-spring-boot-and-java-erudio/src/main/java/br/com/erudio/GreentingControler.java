1c6a4e2c84cce55c839f21b312b00dbfc414adce
PriscilaCavalcanti <priiscila.cavalcanti@gmail.com>
2023-05-16 23:20:39 -0300

    Alteração de parametro

--- 06_ParametersAndExceptionHandler/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/GreentingControler.java
+++ 06_ParametersAndExceptionHandler/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/GreentingControler.java
@@ -11,11 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class GreentingControler {
 	private static final String template = "Hello, %s!";
-	private final AtomicLong counter = new AtomicLong();
+	private final AtomicLong counter = new AtomicLong(); //Mock um ID
 	
 	@RequestMapping("/greenting")
 	public Greenting greenting(@RequestParam(value = "name", defaultValue = "World")String name) {
 		
 		return new Greenting(counter.incrementAndGet(),String.format(template, name));
 	}
+	
+	//Url de Teste Postman : http://localhost:8080/greenting?name=Pri
 }
